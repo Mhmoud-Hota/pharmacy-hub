624efb0b75ba7eadd46a4675b2661f2e065cc197
Mhmoud-Hota <atbarahota@gmail.com>
2026-06-24 16:23:30 +0200

    edit the main to allwo more then 50mg file requst

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,8 @@ import { TransformInterceptor } from './common/interceptors/transform.intercepto
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(require('express').json({ limit: '50mb' }));
+  app.use(require('express').urlencoded({ limit: '50mb', extended: true }));
 
   app.enableCors();
   app.useGlobalFilters(new HttpExceptionFilter());
